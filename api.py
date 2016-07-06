b6c8680e4afa4f166114aa9a510fe90ce807f08b
Stavros Kroustouris <kroustou@gmail.com>
2016-07-06 21:54:59 +0300

    return now playing image link

--- api.py
+++ api.py
@@ -7,6 +7,11 @@ from settings import now_playing_url, discogs_token
 app = Flask(__name__)
 
 
+@app.route("/image/")
+def get_image():
+    if hasattr(app, 'current'):
+        return app.current.get('image')
+
 @app.route("/now-playing/")
 def get_current_song():
     response = requests.get(now_playing_url)
@@ -23,13 +28,13 @@ def get_current_song():
             )[0].get('thumb')
         except:
             image = None
-    return json.dumps(
-        {
+    app.current = {
             'success': response.ok,
             'result': response.text if response.ok else response.status_code,
             'image': image
         }
-    )
+    return json.dumps(app.current)
 
 if __name__ == "__main__":
     app.run()
+    app.current = None
