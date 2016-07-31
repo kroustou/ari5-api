ef7d5cd40e9ab0eb175753cb3684213bbcd48d58
Stavros Kroustouris <kroustou@gmail.com>
2016-07-31 10:51:46 +0300

    history

--- api.py
+++ api.py
@@ -2,31 +2,49 @@
 from flask import Flask
 import requests
 import json
-from settings import now_playing_url, discogs_token
+from bs4 import BeautifulSoup
+from settings import now_playing_url, discogs_token, history_url
 
 app = Flask(__name__)
+
+def get_details(title):
+    image_response = requests.get(
+        "https://api.discogs.com/database/search?q=%s&token=%s" % (
+            title,
+            discogs_token
+        )
+    )
+    try:
+        image = json.loads(image_response.text).get(
+            'results'
+        )[0].get('thumb')
+    except:
+        image = None
+    return {
+        'result': title,
+        'image': image
+    }
+
 
 def get_current_song():
     response = requests.get(now_playing_url)
     if response.ok:
-        image_response = requests.get(
-            "https://api.discogs.com/database/search?q=%s&token=%s" % (
-                response.text,
-                discogs_token
-            )
-        )
-        try:
-            image = json.loads(image_response.text).get(
-                'results'
-            )[0].get('thumb')
-        except:
-            image = None
-    app.current = {
-        'success': response.ok,
-        'result': response.text if response.ok else response.status_code,
-        'image': image
-    }
+        app.current = get_details(response.text)
+    app.current['status'] = response.ok
     return app.current
+
+@app.route("/history/")
+def get_history():
+    response = requests.get(history_url)
+    if response.ok:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        tr = soup.findAll('table')[2].findAll('tr')[2:]
+
+        titles = []
+        for row in tr:
+            titles.append(get_details(row.findAll('td')[1].text))
+        return json.dumps(titles)
+
 
 @app.route("/now-playing/")
 def now_playing():
