74d58c3191001cd988851537a837201f06781e3c
Stavros Kroustouris <kroustou@gmail.com>
2016-08-24 17:31:57 +0300

    failsafe ndiscogs

--- api.py
+++ api.py
@@ -8,13 +8,13 @@ from settings import now_playing_url, discogs_token, history_url, listeners_url
 app = Flask(__name__)
 
 def get_details(title):
-    image_response = requests.get(
-        "https://api.discogs.com/database/search?q=%s&token=%s" % (
-            title,
-            discogs_token
-        )
-    )
     try:
+        image_response = requests.get(
+            "https://api.discogs.com/database/search?q=%s&token=%s" % (
+                title,
+                discogs_token
+            )
+        )
         image = json.loads(image_response.text).get(
             'results'
         )[0].get('thumb')
